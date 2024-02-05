4a29a3cdaa35f31b52e22769d9812edbdd6bfb18
yerragudiasritha <156211704+yerragudiasritha@users.noreply.github.com>
2024-02-05 09:31:13 +0530

    Update main.py

--- Python Assesments/Assignment-5/main.py
+++ Python Assesments/Assignment-5/main.py
@@ -1,9 +1,4 @@
-# task1 = Task1()
-# task1.checkTicketAvailability()
-# task1.calculateTotalCost()
-# task1.looping()
 import mysql.connector
-
 con = mysql.connector.connect(
     host="localhost",
     port="3306",
@@ -11,27 +6,24 @@ con = mysql.connector.connect(
     password="root",
     database="ticketbookingsystem"
 )
-venue = "Harmony Hall"
+venue = "VSK Hall"
 cur = con.cursor()
 cur.execute("select venue_id from venue where venue_name= %s", (venue,))
 venue_id = cur.fetchone()
-
 print(venue_id[0])
 
 """
     event = {
-        "event_name" : input("Enter the event name : "),
-        "event_date" : input("Enter the date of the event : "),
-        "event_time" : input("Enter Event timing : "),
+        "event_name" : input("Enter the event name of the event: "),
+        "event_date" : input("Enter the date : "),
+        "event_time" : input("Enter the Event timing : "),
         "venue_name" : input("Enter venue name : "),
-        "total_seats" : int(input("Enter total seats")),
-        "available_seats" : int(input("Enter available seats : ")),
+        "total_seats" : int(input("Enter total seats in the hall")),
+        "available_seats" : int(input("Enter available seats in the hall: ")),
         "ticket_price" : float(input("Enter the ticket price : ")),
-        "event_type" : input("Enter event type : ")
+        "event_type" : input("Enter type of the event : ")
     }
-    
     booking = Booking(event)
     print(booking.getBookedNoOfTickets())
- #   first_event = Event(event)
-  #  first_event.display_event_details()
+
 """
