0d3049cefd402f734d1efb5dab137629955bf89c
yerragudiasritha <156211704+yerragudiasritha@users.noreply.github.com>
2024-02-06 14:12:11 +0530

    Delete Python coding challenge/hospitalmanagemantsystem directory

--- Python coding challenge/hospitalmanagemantsystem/exceptions/PatientNotFoundException.py
+++
@@ -1,6 +0,0 @@
-class PatientNumberNotFoundException(Exception):
-    def __init__(self, patient_id):
-        self.patient_id = patient_id
-        super().__init__(f" Patient with id {patient_id} not found")
-
-
